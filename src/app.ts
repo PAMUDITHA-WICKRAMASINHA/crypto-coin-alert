09a2fa7e67534f3a4bd31f2f81acfd3cb433fc24
Pamuditha <wickramasinha1219@gmail.com>
2024-09-09 11:15:30 +0000

    CHG | create controller

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 import express, { Application, Request, Response } from 'express';
 import { constants as APP_CONST } from "./constant/application";
+import { processAllFuturesSymbols } from "./controller/RSI.controller";
 
 const app = express();
 
@@ -12,3 +13,10 @@ const PORT = APP_CONST.APP_PORT || 4000;
 app.listen(PORT, (): void => {
     console.log('SERVER IS UP ON PORT:', PORT);
 });
+
+// setInterval(()=>{
+//     // Run the script
+// }, 1000 * 5)
+
+processAllFuturesSymbols();
+
